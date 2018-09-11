da432bd8878ec662683825a7fc91d799a06b7069
Marcelo Franco <marcelofranco.ti@gmail.com>
2018-09-11 17:13:20 -0300

    Add projeto console

--- TesteSeleniumOncoProd/Program.cs
+++ TesteSeleniumOncoProd/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TesteSeleniumOncoProd
@@ -19,100 +20,84 @@ namespace TesteSeleniumOncoProd
 
         public static void AcessarSite()
         {
-            for (int i = 0; i < 100; i++)
-            {
+
+            ChromeDriver _chromeDriver = new ChromeDriver();
+
+            _chromeDriver.Manage().Window.Maximize();
+
+            _chromeDriver.Navigate().GoToUrl(new Uri("http://localhost:4395/Login.aspx"));
+
+            IWebElement usuario = _chromeDriver.FindElement(By.Id("txtLogin"));
+            usuario.SendKeys("THIAGOA");
+
+            IWebElement senha = _chromeDriver.FindElement(By.Id("txtPassword"));
+            senha.SendKeys("oncoks");
+
+            IWebElement button = _chromeDriver.FindElement(By.Id("btnLogin"));
+            button.Click();
+
+            Thread.Sleep(2000);
+            _chromeDriver.Navigate().GoToUrl(new Uri("http://localhost:4395/AppPaginas/Cadastros/CadCheckListLicitatorio.aspx"));
 
 
-                ChromeDriver _chromeDriver = new ChromeDriver();
+            IWebElement novoItem = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_btnFiltrosNovoCadastro"));
+            novoItem.Click();
 
-                _chromeDriver.Manage().Window.Maximize();
+            IWebElement CNPJ = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_txtCnpjIclusao"));
+            CNPJ.SendKeys("67.838.490/0001-70");
 
-                _chromeDriver.Navigate().GoToUrl(new Uri("http://localhost:4395/Login.aspx"));
+            IWebElement checkCliente = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_imbContratoHeaderCnpjCheck"));
+            checkCliente.Click();
 
-                _chromeDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromMinutes(10));
+            IWebElement CODE = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_txtCadCodigo"));
+            CODE.SendKeys("670001");
 
-                IWebElement usuario = _chromeDriver.FindElement(By.Id("txtLogin"));
-                usuario.SendKeys("THIAGOA");
+            IWebElement vigencia = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_txtVigencia"));
+            vigencia.SendKeys("20");
 
-                _chromeDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromMinutes(10));
-
-                IWebElement senha = _chromeDriver.FindElement(By.Id("txtPassword"));
-                senha.SendKeys("oncoks");
-
-                IWebElement button = _chromeDriver.FindElement(By.Id("btnLogin"));
-                button.Click();
-
-                _chromeDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromMinutes(50000));
-
-
-                _chromeDriver.Navigate().GoToUrl(new Uri("http://localhost:4395/AppPaginas/Cadastros/CadCheckListLicitatorio.aspx"));
-
-                IWebElement novoItem = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_btnFiltrosNovoCadastro"));
-                novoItem.Click();
-
-                IWebElement CNPJ = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_txtCnpjIclusao"));
-                CNPJ.SendKeys("67.838.490/0001-70");
-
-                IWebElement checkCliente = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_imbContratoHeaderCnpjCheck"));
-                checkCliente.Click();
-
-
-                _chromeDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromMinutes(50000));
-                _chromeDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromMinutes(50000));
-
-                _chromeDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromMinutes(50000));
-                _chromeDriver.Quit();
-            }
-            //IWebElement CODE = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_txtCadCodigo"));
-            //CODE.SendKeys("670001");
-
-            //IWebElement vigencia = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_txtVigencia"));
-            //CODE.SendKeys("20");
-
-            //IWebElement prazo = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_txtPrazoEntrega"));
-            //CODE.SendKeys("30");
+            IWebElement prazo = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_txtPrazoEntrega"));
+            prazo.SendKeys("30");
 
 
 
-            //
+            IWebElement dtAbertura = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_dtAbertura_txtData"));
+            dtAbertura.SendKeys("11/09/2018");
 
-            //_chromeDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromMinutes(10));
+            IWebElement dtCredenc = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_dtCredenc_txtData"));
+            dtCredenc.SendKeys("12/09/2018");
 
-            //_chromeDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromMinutes(500));
+            IWebElement dtRetomada = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_dtRetomada_txtData"));
+            dtRetomada.SendKeys("13/09/2018");
 
-            //IWebElement bater = _chromeDriver.FindElement(By.Id("btnFecharModalBaterPonto"));
-            //bater.Click();
+            IWebElement dtLimite = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_dtLimite_txtData"));
+            dtLimite.SendKeys("13/09/2018");
 
-            //_chromeDriver.Quit();
+            IWebElement dtExigida = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_dtExigida_txtData"));
+            dtExigida.SendKeys("13/09/2018");
 
-            //EnviarEmail();
-            //btnIncluirBatidaModalBaterPonto
-            //btnFecharModalBaterPonto
+
+            IWebElement item1 = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_txtPesquisarItem"));
+            item1.SendKeys("00075");
+
+            IWebElement clickItem = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_imgBuscaItensCheck"));
+            clickItem.Click();
+            Thread.Sleep(2000);
+            item1.SendKeys("00074");
+            clickItem.Click();
+            Thread.Sleep(2000);
+
+            item1.SendKeys("00074");
+            clickItem.Click();
+            Thread.Sleep(2000);
+
+            item1.SendKeys("00077");
+            clickItem.Click();
+            Thread.Sleep(2000);
+
+
+            IWebElement salvar = _chromeDriver.FindElement(By.Id("ContentPlaceHolder1_btnSalvar"));
+            salvar.Click();
 
         }
-
-        //public static void EnviarEmail()
-        //{
-        //    SmtpClient SmtpClient;
-
-        //    MailMessage MailMessage;
-
-        //    SmtpClient = new SmtpClient();
-        //    SmtpClient.Host = "smtp.gmail.com";
-        //    SmtpClient.Port = 587;
-        //    SmtpClient.EnableSsl = true;
-        //    SmtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-        //    // cria uma mensagem
-        //    MailMessage = new MailMessage("marcelofranco.ti@gmail.com", "marcelofranco.ti@gmail.com", "Ponto batido", "Seu ponto foi batido as " + DateTime.Now.ToString());
-
-        //    SmtpClient.EnableSsl = true;
-
-        //    SmtpClient.UseDefaultCredentials = false;
-        //    NetworkCredential cred = new NetworkCredential("marcelofranco.ti@gmail.com", "M@rcelo1988");
-        //    SmtpClient.Credentials = cred;
-
-        //    // envia a mensagem
-        //    SmtpClient.Send(MailMessage);
-        //}
     }
 }
